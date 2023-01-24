ee263ba42ab07db04d88ccaae2f5669cebfade0e
aswanth-pk <aswanthpkallai@gmail.com>
2023-01-24 15:36:15 +0530

    first commit

---
+++ src/Components/LoadingScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LoadingScreen = () => {
+  return (
+    <div>
+        <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+  )
+}
+
+export default LoadingScreen
